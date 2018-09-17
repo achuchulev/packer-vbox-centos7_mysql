23e33575212ac062cee9a46d1241f4f1564d8b62
achuchulev <42144722+achuchulev@users.noreply.github.com>
2018-09-17 15:24:25 +0300

    Update mysql_check.rb

--- test/integration/default/mysql_check.rb
+++ test/integration/default/mysql_check.rb
@@ -1,4 +1,11 @@
+# Use the service InSpec audit resource to test if the named service is installed, running and/or enabled.
 describe service('mysqld') do
+  it { should be_installed }
   it { should be_enabled }
   it { should be_running }
+end
+
+# Test a parameter set within the [mysqld] section
+describe mysql_conf do
+  its('mysqld.port') { should cmp 3306 }
 end
