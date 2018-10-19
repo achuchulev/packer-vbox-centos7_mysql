1f5e400d42608bd1d8a0de2d3de5e03304d85782
Atanas Chuchulev <achuchulev@Atanass-MacBook-Pro.local>
2018-10-19 10:52:26 +0300

    new control checks added

--- Vagrantfile
+++ Vagrantfile
@@ -1,3 +1,4 @@
 Vagrant.configure("2") do |config|
-  config.vm.box = "mysql-box"
+  config.vm.box = "achuchulev/centos7_mysql"
+  config.vm.box_version = "0.0.1"
 end
--- test/integration/default/mysql_check.rb
+++
@@ -1,6 +0,0 @@
-# Use the service InSpec audit resource to test if the named service is installed, running and/or enabled.
-describe service('mysqld') do
-  it { should be_installed }
-  it { should be_enabled }
-  it { should be_running }
-end
